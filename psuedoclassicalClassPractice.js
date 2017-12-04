34dc3bb7aa43b74e3629d375685824ebb72ac3d9
Brady Espinosa <brady@Bradys-MacBook-Pro.local>
2017-12-04 15:32:55 -0800

    tried es6 class vs psuedoclassical to compare

--- psuedoclassicalClassPractice.js
+++ psuedoclassicalClassPractice.js
@@ -1,3 +1,5 @@
+//pseudoclassical version
+
 function apartment(bedrooms) {
 	this.what = 'apartment';
 	this.bedrooms = bedrooms;
@@ -7,6 +9,13 @@ apartment.prototype.countRooms = function() {
 	console.log('I have', this.bedrooms, 'bedrooms');
 };
 
+apartment.prototype.getBigger = function() {
+	if (this.bedrooms < 5) {
+		console.log('tiny homes are hip');
+	}
+};
+
 var myPlace = new apartment(3);
 
 myPlace.countRooms();
+myPlace.getBigger();
